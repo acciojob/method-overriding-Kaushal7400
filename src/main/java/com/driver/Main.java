d00c01bdd74da015995d1741bf8c2d694976b6f2
Kaushalendra singh <ksb250998@gmail.com>
2024-01-22 09:28:11 +0530

    override 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String args[]) {
         A obj = new B();
-        System.out.println(obj.meth());
+        obj.meth();
     }
 }
 
