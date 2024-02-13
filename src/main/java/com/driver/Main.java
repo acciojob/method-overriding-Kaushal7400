4290a625733d4675f19ce3fc5c5e6a37a5ae9405
Kaushalendra singh <ksb250998@gmail.com>
2024-02-13 11:40:53 +0530

    overriding 333

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,25 @@
 package com.driver;
 
-public class Main {
-    public static void main(String args[]) {
-        B obj = new B();
-        obj.meth();
-    }
-}
-
- class A {
-     String meth() {
-         return "Invoking method from class A";
-     }
- }
- class B extends A{
+       class A {
+         String meth() {
+             return "Invoking method from class A";
+         }
+       }
+       class B extends A {
+             String meth() {
+                 return "Method is overridden in Extendend class B";
+             }
 
 
-    String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
+             public static void main(String args[]) {
+                 B obj1 = new B();
+                 System.out.print(obj1.meth());
+
+                 B obj2 = new B();
+                 System.out.print(obj2.meth());
+             }
+       }
+
 
 
 
